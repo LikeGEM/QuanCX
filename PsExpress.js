13d4247df3776976f65bee097d78ebed099e2f79
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2022-11-30 00:47:51 +0800

    Create PsExpress.js

---
+++ PsExpress.js
@@ -0,0 +1,19 @@
+/***********************************
+            
+# Ps Express
+
+[rewrite_local]
+
+^https:\/\/lcs-mobile-cops\.adobe\.io\/mobile_profile url script-response-body https://raw.githubusercontent.com/LikeGEM/QuanCX/main/PsExpress.js
+
+[mitm] 
+hostname= lcs-mobile-cops.adobe.io, photos.adobe.io
+
+***********************************/
+
+let obj = JSON.parse($response.body)
+let pro= obj["mobileProfile"];
+pro["profileStatus"] = "PROFILE_AVAILABLE";
+pro["legacyProfile"] = "{}";
+pro["relationshipProfile"] = "[]";
+$done({body: JSON.stringify(obj)})

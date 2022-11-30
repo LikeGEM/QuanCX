5f5d55456f6b830a6f1de797507a15391278e3db
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2022-11-30 19:30:16 +0800

    Create shoutushenqi.js

---
+++ shoutushenqi.js
@@ -0,0 +1,8 @@
+# 搜图神器
+
+[rewrite_local]
+
+http:\/\/wallpaper\.soutushenqi\.com\/api\/v1\/account\/token url response-body vipType": 0 response-body vipType": 1024
+
+[mitm] 
+hostname= wallpaper.soutushenqi.com

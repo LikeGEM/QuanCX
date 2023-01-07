626bf82443fdb93f671330ccf5a9b228c7aff2d8
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2023-01-08 07:53:40 +0800

    Update jianying.js

--- jianying.js
+++ jianying.js
@@ -6,6 +6,8 @@
 
 ^http[s]?:\/\/commerccd-api.faceu.mobi\/commerce\/v1\/subscription\/user_info.*$ url script-request-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/Xuid.js
 
+^http[s]?:\/\/.+((retouchpics)|(faceu)|(ulikecam)).(com|cn|mobi)\/.+(user|\/v1\/homepage\/pro|ever_photo/space_info).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/Xtu.js
+
 [mitm] 
 
 hostname= *.ulikecam.*, *faceu*

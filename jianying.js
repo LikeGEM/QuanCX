8c166251af07e556c4fac0d54b808c1779eace21
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2023-01-08 07:41:56 +0800

    Create jianying.js

---
+++ jianying.js
@@ -0,0 +1,13 @@
+/***********************************
+
+#醒图
+
+[rewrite_local]
+
+^http[s]?:\/\/commerccd-api.faceu.mobi\/commerce\/v1\/subscription\/user_info.*$ url script-request-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/Xuid.js
+
+[mitm] 
+
+hostname= *.ulikecam.*
+
+***********************************/

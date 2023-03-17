683c6ac8c5810386028b14b17a26757a5121eca2
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2023-03-17 13:10:25 +0800

    Update Nicegram.js

--- Nicegram.js
+++ Nicegram.js
@@ -1,7 +1,7 @@
 /******************************
 #Nicegram
 [rewrite_local]
-^https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/LikeGEM/QuanCX/main/Nicegram.js
+https?:\/\/restore-access\.indream\.app\/restoreAccess\?id=\d{5,10} url echo-response text/json echo-response https://raw.githubusercontent.com/LikeGEM/QuanCX/main/Nicegram.js
 
 [mitm] 
 hostname = restore-access.indream.app

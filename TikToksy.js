8bc9466ac95a914f0d8d04475eb323b8aa3bf70a
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2023-06-12 17:10:12 +0800

    Update TikToksy.js

--- TikToksy.js
+++ TikToksy.js
@@ -1,14 +1,3 @@
-/*************************************
-
-TikTok去水印
-[rewrite_local]
-https?:\/\/.*\.tiktokv\.com\/aweme\/v\d\/(feed|mix\/aweme|aweme\/post|(multi\/)?aweme\/detail|follow\/feed|nearby\/feed|search\/item|general\/search\/single|hot\/search\/video\/list|aweme\/favorite) url script-response-body https://raw.githubusercontent.com/LikeGEM/QuanCX/main/TikToksy.js
-[mitm]
-
-hostname = *.tiktokv.com,*.byteoversea.com, *.tik-tokapi.com
-
-*************************************/
-
 var watermark = body => {
     try {
         body.replace(/\"room_id\":(\d{2,})/g, '"room_id":"$1"');

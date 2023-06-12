2276c56bcfe8210a49f67048d1b4773e49e6861f
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2023-06-12 14:16:13 +0800

    Update TikToksy.js

--- TikToksy.js
+++ TikToksy.js
@@ -2,7 +2,7 @@
 
 TikTok去水印
 [rewrite_local]
-^https?:\/\/.*\.tiktokv\.com\/aweme\/v\d\/(feed|mix\/aweme|aweme\/post|(multi\/)?aweme\/detail|follow\/feed|nearby\/feed|search\/item|general\/search\/single|hot\/search\/video\/list|aweme\/favorite) url script-response-body https://raw.githubusercontent.com/LikeGEM/QuanCX/main/TikToksy.js
+https?:\/\/.*\.tiktokv\.com\/aweme\/v\d\/(feed|mix\/aweme|aweme\/post|(multi\/)?aweme\/detail|follow\/feed|nearby\/feed|search\/item|general\/search\/single|hot\/search\/video\/list|aweme\/favorite) url script-response-body https://raw.githubusercontent.com/LikeGEM/QuanCX/main/TikToksy.js
 [mitm]
 
 hostname = *.tiktokv.com,*.byteoversea.com, *.tik-tokapi.com

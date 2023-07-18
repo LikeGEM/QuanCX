b8f8717fee9ff399769022c062c08e6980ae8d38
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2023-07-19 01:36:27 +0800

    Create pppp.js

---
+++ pppp.js
@@ -0,0 +1,5 @@
+[rewrite_local]
+^https:\/\/access1\.imglarger\.com\/PhoAi\/GetHistoryList url script-response-body https://raw.githubusercontent。com/LikeGEM/QuanCX/main/photo.js
+
+[mitm]
+hostname = access1.imglarger。com

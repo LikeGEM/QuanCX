b791507f89856cd32f6358697f75a863ac38a7a7
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2023-07-19 01:58:31 +0800

    Update pppp.js

--- pppp.js
+++ pppp.js
@@ -1,5 +1,5 @@
 [rewrite_local]
-^https:\/\/access1\.imglarger\.com\/PhoAi\/GetHistoryList url script-response-body https://raw.githubusercontent。com/LikeGEM/QuanCX/main/photo.js
+^https:\/\/access1\.imglarger\.com\/PhoAi\/GetHistoryList url script-response-body https://raw.githubusercontent.com/LikeGEM/QuanCX/main/photo.js
 
 [mitm]
 hostname = access1.imglarger。com

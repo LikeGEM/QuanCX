f109c3b3c15cdfec609e8baed189be281487f526
LikeGEM <107976555+LikeGEM@users.noreply.github.com>
2023-07-19 01:32:01 +0800

    Create photo.js

---
+++ photo.js
@@ -0,0 +1,19 @@
+{
+  "msg" : "Success"，
+  "data" : [
+    {
+      "status" : "not foundnot found"，
+      "userName" : "D709AE6E-3AD7-44F3-B2C4-24115BDE016A"，
+      "id" : 518180，
+      "code" : "3PJYO2KE"，
+      "aiecreateDate" : "2023-07-17 18:47"，
+      "scaleRadio" : 0，
+      "typeName" : "Upscaler"，
+      "createDate" : "17 Jul"，
+      "type" : 9，
+      "bgeraserCode" : null，
+      "url" : "https://get1.imglarger.com/results/3PJYO2KE_4x.jpg"
+    }
+  ]，
+  "code" : 200
+}
